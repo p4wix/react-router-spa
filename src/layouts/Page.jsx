24208ff57200d8b76ae2e04668335f8aa15cfab9
Pawel <pawix@gmail.com>
2021-04-27 23:43:32 +0200

    layout

--- src/layouts/Page.jsx
+++ src/layouts/Page.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 
 const Page = () => {
-	return <div>Page</div>;
+	return (
+		<Switch>
+			<Route path="/" exact component={HomePage} />;
+			<Route path="/" exact component={ProductPage} />;
+			<Route path="/" exact component={ContactPage} />;
+			<Route path="/" exact component={AdminPage} />;
+			<Route path="/" exact component={ErrorPage} />;
+		</Switch>
+	);
 };
 
 export default Page;
