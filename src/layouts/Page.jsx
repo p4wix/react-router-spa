5d7ade2152c7b279c9bc28512178ef6cc490c869
Pawel <pawix@gmail.com>
2021-04-27 23:48:19 +0200

    create pages

--- src/layouts/Page.jsx
+++ src/layouts/Page.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 
+import ProductPage from "../pages/ProductPage";
+import ContactPage from "../pages/ContactPage";
+import AdminPage from "../pages/AdminPage";
+import ErrorPage from "../pages/ErrorPage";
+
 const Page = () => {
 	return (
 		<Switch>
 			<Route path="/" exact component={HomePage} />;
-			<Route path="/" exact component={ProductPage} />;
-			<Route path="/" exact component={ContactPage} />;
-			<Route path="/" exact component={AdminPage} />;
-			<Route path="/" exact component={ErrorPage} />;
+			<Route path="/products" component={ProductPage} />;
+			<Route path="/contact" component={ContactPage} />;
+			<Route path="/admin" component={AdminPage} />;
+			<Route component={ErrorPage} />;
 		</Switch>
 	);
 };
